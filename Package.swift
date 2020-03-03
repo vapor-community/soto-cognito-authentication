88dd274ebf494af1c2f4632b97407e13964594de
Adam Fowler <adamfowler71@gmail.com>
2020-03-03 18:52:11 +0000

    Update to Vapor 4.0.0-rc
    Update to swift 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,23 +14,28 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/swift-aws/aws-sdk-swift.git", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "4.0.0-beta.3")),
-        .package(url: "https://github.com/vapor/jwt-kit.git", .upToNextMajor(from: "4.0.0-beta.2.1")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "4.0.0-rc.1")),
+        .package(url: "https://github.com/vapor/jwt-kit.git", .upToNextMajor(from: "4.0.0-rc.1")),
         // for SRP
         .package(url: "https://github.com/adam-fowler/big-num.git", .upToNextMajor(from: "1.1.0")),
     ],
     targets: [
-        .target(name: "AWSCognitoAuthentication", dependencies: ["AWSCognitoAuthenticationKit", "Vapor"]),
-        .target(name: "AWSCognitoAuthenticationKit",
-                dependencies: [
-                    "AsyncHTTPClient",
-                    "BigNum",
-                    "CognitoIdentity",
-                    "CognitoIdentityProvider",
-                    "JWTKit",
-                    "Crypto"
+        .target(name: "AWSCognitoAuthentication",
+            dependencies: [
+                .target(name: "AWSCognitoAuthenticationKit"),
+                .product(name: "Vapor", package: "vapor")
             ]
         ),
-        .testTarget(name: "AWSCognitoAuthenticationKitTests", dependencies: ["AWSCognitoAuthenticationKit"]),
+        .target(name: "AWSCognitoAuthenticationKit",
+                dependencies: [
+                    .product(name: "AsyncHTTPClient", package: "async-http-client"),
+                    .product(name: "BigNum", package: "big-num"),
+                    .product(name: "CognitoIdentity", package: "aws-sdk-swift"),
+                    .product(name: "CognitoIdentityProvider", package: "aws-sdk-swift"),
+                    .product(name: "JWTKit", package: "jwt-kit"),
+                    .product(name: "Crypto", package: "swift-crypto")
+            ]
+        ),
+        .testTarget(name: "AWSCognitoAuthenticationKitTests", dependencies: [.target(name: "AWSCognitoAuthenticationKit")]),
     ]
 )
